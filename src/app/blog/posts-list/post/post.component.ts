1ab3fbef0f37db91085ae678504a597723c235e7
Andres Duarte Rey <duranduarte@google.com>
2020-02-11 14:37:41 +0000

    dsfsf

--- src/app/blog/posts-list/post/post.component.ts
+++ src/app/blog/posts-list/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SecurityContext, Injector, ViewChild, ComponentFactoryResolver, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, SecurityContext, Injector, ViewChild, ComponentFactoryResolver, ChangeDetectionStrategy, ChangeDetectorRef, ElementRef, Renderer2, ViewContainerRef, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Post } from '../../post.model';
 import { BlogService } from '../../blog.service';
@@ -22,15 +22,23 @@ export class PostComponent implements OnInit {
   author: Author;
   postFormattedDate: string;
   product: Product;
-   @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+/*    @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+   @ViewChild('content', { static: false, read: ViewContainerRef}) content: ViewContainerRef;
+   @ViewChild('content2', { static: false, read: ViewContainerRef}) content2: ViewContainerRef;
+
+   @ViewChild('test', { static: false, read: TemplateRef}) test: TemplateRef<any>;
+
+   safeContent;
    productItemRef;
    productItemRef2;
-  private closeSub: Subscription; 
+   nativeContentElement;
+  private closeSub: Subscription;  */
 
 
 
   constructor(private activatedRoute: ActivatedRoute, private blogService: BlogService,
-    private productsService: ProductsService, private router: Router, private cdr: ChangeDetectorRef, private componentFactoryResolver: ComponentFactoryResolver
+    private productsService: ProductsService, private router: Router, private cdr: ChangeDetectorRef, 
+    private componentFactoryResolver: ComponentFactoryResolver, private renderer: Renderer2
     ) { }
 
   ngOnInit() {
@@ -46,6 +54,12 @@ export class PostComponent implements OnInit {
         this.postFormattedDate = new Date(this.post.date * 1000).toLocaleString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
       }
     );
+
+/*     this.safeContent.innerHTML = `
+    <p>introduction</p>
+    <div id="product1"></div>
+    <button>button text</button>
+  `; */
   }
 
   nextArticle() {
@@ -60,36 +74,43 @@ export class PostComponent implements OnInit {
   }
 
   ngAfterViewInit(){
-
-    let content = document.createElement('div');
-    content.innerHTML = `
-    <p>introduction</p>
-    <app-product-item>more text</app-product-item>
-    <app-product-item>and more text</app-product-item>
-    <button>button text</button>
-  `;
-
-
-
+/* 
      const productItemCmpFactory = this.componentFactoryResolver.resolveComponentFactory(ProductItemComponent);
     const hostViewContainerRef = this.alertHost.viewContainerRef;
     hostViewContainerRef.clear();
     //this.productItemRef = hostViewContainerRef.createComponent(productItemCmpFactory);
-    this.productItemRef = hostViewContainerRef.createComponent(productItemCmpFactory, undefined, undefined, [[content]]);
 
-    this.product =  this.productsService.getProductById(0);
-    this.productItemRef.instance.product = this.product;
+    this.productItemRef = hostViewContainerRef.createComponent(productItemCmpFactory, undefined, undefined, [[content]]);
+    let product =  this.productsService.getProductById(0);
+    this.productItemRef.instance.product = product;
 
     this.productItemRef2 = hostViewContainerRef.createComponent(productItemCmpFactory, undefined, undefined, [[content]]);
+    let product2 =  this.productsService.getProductById(1);
+    this.productItemRef2.instance.product = product2;
 
-    this.product =  this.productsService.getProductById(1);
-    this.productItemRef.instance.product = this.product;
+    this.nativeContentElement = this.alertHost.viewContainerRef.element.nativeElement;
 
+    //var node = document.createElement("div");  
+    //console.log(node)               // Create a <li> node
+//node.innerHTML = 'test satu dua tiga';
+// Create a text node
+//this.nativeContentElement.appendChild(node);
+//this.content.el.nativeElement.appendChild(node);
+//@ViewChild('content2', { static: false, read: ViewContainerRef}) content2: ViewContainerRef;
+@ViewChild('product1', { static: false}) let product1: ElementRef;
 
+const p = this.renderer.createElement('p');
+const text = this.renderer.createText('Click here to add li');
+this.renderer.appendChild(p, text);
+this.renderer.appendChild(product1.nativeElement, p);
 
+//this.content.createEmbeddedView(this.test);
+//this.content.createEmbeddedView(this.content2.element);
+
+let content = document.createElement('<div>fddksandkldsn</div>');
 
     this.cdr.detectChanges(); 
-
+ */
   }
 
 /*   ngOnDestroy() {
